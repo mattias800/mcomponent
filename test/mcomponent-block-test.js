4482eadcf6f50da510e8323363229dd5ab055c7d
Mattias Andersson <mattias800@gmail.com>
2013-03-26 16:46:31 +0100

    More test refactoring.

--- test/mcomponent-block-test.js
+++ test/mcomponent-block-test.js
@@ -1,60 +1,71 @@
 TestCase("Find blocks", {
 
-    "test Find block end" : function() {
-
-        var c;
-
-        c = mcomponent({viewHtml : "{{ name }}"});
-
-        assertTrue("List size is 1.", c.assert.assertListSize(1));
+    "test findBlockEnd() throws exception when trying to find block end for a tag that doesn't open a block" : function() {
+        var c = mcomponent({viewHtml : "{{ name }}{{ name }}"});
+        assertTrue(c.assert.assertListSize(2));
         assertException("Not tag with block, throw exception", function() {
             c._.findBlockEnd(0)
         });
+    },
 
-
+    "test throws exception when missing endif" : function() {
         assertException("Throws error since template is malformed.", function() {
-            c = mcomponent({
+            mcomponent({
                 viewHtml : "{{ if name }}",
                 throwOnError : true
             });
         });
 
-        assertTrue("List size is 1.", c.assert.assertListSize(1));
+    },
+
+    "test findBlockEnd() throws exception when list is to short to have a block" : function() {
+        var c = mcomponent({viewHtml : "{{ name }}"});
+        assertTrue(c.assert.assertListSize(1));
         assertException("Exception, list is too short.", function() {
             c._.findBlockEnd(0)
         });
+    },
 
-        c = mcomponent({viewHtml : "before{{ if aaaname }}inside{{ endif }}after"});
+    "test findBlockEnd() finds endif after if with inner and outer HTML" : function() {
+        var c = mcomponent({viewHtml : "before{{ if aaaname }}inside{{ endif }}after"});
         assertTrue(c.assert.assertListSize(5));
         assertEquals(c._.findBlockEnd(1), 3);
+    },
 
-        c = mcomponent({viewHtml : "{{ if supername }}test{{ endif }}"});
-        assertTrue("List size is 3.", c.assert.assertListSize(3));
-        assertEquals("Should find ending tag on index 2.", 2, c._.findBlockEnd(0));
+    "test findBlockEnd() finds endif after if with inner HTML" : function() {
+        var c = mcomponent({viewHtml : "{{ if supername }}test{{ endif }}"});
+        assertTrue(c.assert.assertListSize(3));
+        assertEquals(2, c._.findBlockEnd(0));
+    },
 
-        c = mcomponent();
+    "test findBlockEnd() finds outer ifs endif even though inner if tag is missing endif" : function() {
+        var c = mcomponent();
         assertTrue(c.assert.assertSetViewAndBuildList("{{ if thename }}{{ if age }}test{{ endif }}"));
-        assertTrue("List size is 4.", c.assert.assertListSize(4));
+        assertTrue(c.assert.assertListSize(4));
+        assertEquals("Should find ending tag on index 3.", 3, c._.findBlockEnd(1));
+    },
+
+    "test findBlockEnd() throws exception when not finding the second endif tag" : function() {
+        var c = mcomponent();
+        assertTrue(c.assert.assertSetViewAndBuildList("{{ if thename }}{{ if age }}test{{ endif }}"));
+        assertTrue(c.assert.assertListSize(4));
         assertException("Should not find a closing endif.", function() {
             c._.findBlockEnd(0);
         });
-        assertEquals("Should find ending tag on index 3.", 3, c._.findBlockEnd(1));
+    },
 
-        c = mcomponent({viewHtml : "{{ if name }}{{ if age }}test{{ endif }}{{ endif }}"});
+    "test findBlockEnd() find endif for both outer and inner if tag" : function() {
+        var c = mcomponent({viewHtml : "{{ if name }}{{ if age }}test{{ endif }}{{ endif }}"});
         assertTrue("List size is 5.", c.assert.assertListSize(5));
         assertEquals("Should find ending tag on index 4.", 4, c._.findBlockEnd(0));
         assertEquals("Should find ending tag on index 3.", 3, c._.findBlockEnd(1));
+    },
 
-        c = mcomponent({viewHtml : "{{ if name }}{{ push age }}test{{ endpush }}{{ endif }}"});
+    "test findBlockEnd() finds end tag for both outer if and inner push" : function() {
+        var c = mcomponent({viewHtml : "{{ if name }}{{ push age }}test{{ endpush }}{{ endif }}"});
         assertTrue("List size is 5.", c.assert.assertListSize(5));
         assertEquals("Should find ending tag on index 4.", 4, c._.findBlockEnd(0));
         assertEquals("Should find ending tag on index 3.", 3, c._.findBlockEnd(1));
-
-        c = mcomponent({viewHtml : "{{ if (this.model.name) }}{{ name }}{{ endif }}"});
-        assertTrue("List size is 3.", c.assert.assertListSize(3));
-        assertEquals("Should find ending tag on index 2.", 2, c._.findBlockEnd(0));
-
-
     },
 
     "test findBlockEnd() for if-else cases with no HTML" : function() {
